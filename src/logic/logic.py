46a72c61148f271e48b4cc5042c9927fa5e44b4a
Kevincue12 <kevin.a.cuevas.p@hotmail.com>
2025-08-19 00:35:09 -0500

    7

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -91,7 +91,8 @@ class Logica:
         Returns:
             bool: Resultado de a XNOR b
         """
-        pass
+        return not ((a or b) and not (a and b))
+
     
     def implicacion(self, a, b):
         """

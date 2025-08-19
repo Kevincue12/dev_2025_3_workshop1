c126521b8018765a787a957b6958b679fe9e59c0
Kevincue12 <kevin.a.cuevas.p@hotmail.com>
2025-08-18 23:39:21 -0500

    3

--- src/games/games.py
+++ src/games/games.py
@@ -70,20 +70,19 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
-        hay_espacios = any(" " in fila for fila in tablero)
-        
         for i in range(3):
             if tablero[i][0] == tablero[i][1] == tablero[i][2] != " ":
-                return tablero[i][0] if not hay_espacios else "continua"
+                return tablero[i][0]
             if tablero[0][i] == tablero[1][i] == tablero[2][i] != " ":
-                return tablero[0][i] if not hay_espacios else "continua"
+                return tablero[0][i]
         
-        # Verificar diagonales
         if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
-            return tablero[0][0] if not hay_espacios else "continua"
+            return tablero[0][0]
         if tablero[0][2] == tablero[1][1] == tablero[2][0] != " ":
-            return tablero[0][2] if not hay_espacios else "continua"
+            return tablero[0][2]
         
+        # Verificar si hay espacios vacíos
+        hay_espacios = any(" " in fila for fila in tablero)
         if hay_espacios:
             return "continua"
         

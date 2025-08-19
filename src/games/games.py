9727be91992f2e79700a60fd1fc8f181d81bd174
Kevincue12 <kevin.a.cuevas.p@hotmail.com>
2025-08-18 23:37:27 -0500

    3

--- src/games/games.py
+++ src/games/games.py
@@ -70,20 +70,22 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
+        hay_espacios = any(" " in fila for fila in tablero)
+        
         for i in range(3):
             if tablero[i][0] == tablero[i][1] == tablero[i][2] != " ":
-                return tablero[i][0]
+                return tablero[i][0] if not hay_espacios else "continua"
             if tablero[0][i] == tablero[1][i] == tablero[2][i] != " ":
-                return tablero[0][i]
+                return tablero[0][i] if not hay_espacios else "continua"
         
+        # Verificar diagonales
         if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
-            return tablero[0][0]
+            return tablero[0][0] if not hay_espacios else "continua"
         if tablero[0][2] == tablero[1][1] == tablero[2][0] != " ":
-            return tablero[0][2]
+            return tablero[0][2] if not hay_espacios else "continua"
         
-        for fila in tablero:
-            if " " in fila:
-                return "continua"
+        if hay_espacios:
+            return "continua"
         
         return "empate"
     

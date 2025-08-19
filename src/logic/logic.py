4c90f6842f4caf5e2e99873347088c78d3a8a7bb
Kevincue12 <kevin.a.cuevas.p@hotmail.com>
2025-08-19 00:35:42 -0500

    8

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -105,7 +105,7 @@ class Logica:
         Returns:
             bool: Resultado de la implicación
         """
-        pass
+        return (not a) or b
     
     def bi_implicacion(self, a, b):
         """
